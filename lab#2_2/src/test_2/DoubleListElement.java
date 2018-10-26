563d7850320d4abba463ef93103dc997a08fc519
mingchipc <priscilla199685@gmail.com>
2018-10-26 13:42:27 -0600

    Adding again

--- lab#2_2/src/test_2/DoubleListElement.java
+++ lab#2_2/src/test_2/DoubleListElement.java
@@ -9,11 +9,95 @@ package test_2;
  */
 public class DoubleListElement {
 
+	
+	public DoubleListElement next;
+	public DoubleListElement previous;
+	private int data;
+	private DoubleListElement head;
+	private DoubleListElement tail;
+	
+	public DoubleListElement() {
+		head = null;
+		tail = null;
+	}
+
+	public void setData(int data) {
+		this.data=data;
+	}
+	
+	public int getData() {
+		return this.data;
+	}
+	
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	public void addElement1(DoubleListElement le) {
+		
+		DoubleListElement newone = new DoubleListElement(le);
+		
+		if(isEmpty()) {
+			newone.next = null;
+			newone.previous = null;
+			head = newone;
+			tail = newone;
+		}
+		else {
+			head.previous = newone;
+			newone.next = head;
+			newone.previous = null;
+			head = newone;
+		}
+	}
+	
+	public void addElement2(DoubleListElement le) {
+		
+		DoubleListElement newone2 = new DoubleListElement(le);
+		
+		if(isEmpty()) {
+			newone2.next = null;
+			newone2.previous = null;
+			head = newone2;
+			tail = newone2;
+		}
+		else {
+			tail.previous = newone2;
+			newone2.next = tail;
+			newone2.previous = null;
+			head = newone2;
+		}
+	}
+	
+	public void printLinkedListTail() {
+		
+		DoubleListElement curr = tail;
+		while(curr != null) {
+			System.out.print(curr.data+" ");  
+			curr = curr.previous;
+		}
+		
+	}
+	
+	public void printLinkedListHead() {
+		
+		DoubleListElement curr = head;
+		while(curr != null) {
+			System.out.print(curr.data+" ");  
+			curr = curr.next;
+		}
+		
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		DoubleListElement le = new DoubleListElement();
+		le.setData(5);
+
 
 	}
 
