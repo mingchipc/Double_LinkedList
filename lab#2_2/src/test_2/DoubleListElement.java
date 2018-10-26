35e9ae5ccf3b22addd097ce9bf5306bc9e9a5288
mingchipc <priscilla199685@gmail.com>
2018-10-26 13:34:36 -0600

    Add

---
+++ lab#2_2/src/test_2/DoubleListElement.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test_2;
+
+/**
+ * @author tsai203m
+ *
+ */
+public class DoubleListElement {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
